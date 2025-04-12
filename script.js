6f026c3f227d9a1dbc9380972bf7d668d5bb0dc2
uzairit <uzair18it@gmail.com>
2025-04-12 12:49:25 +0500

    The ClassList object has been used.

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ let input = document.querySelector('input');
 btn.addEventListener('click', () => {
     if (btn.innerText === 'Show Password') {
         input.type = 'text';
+        btn.classList.add('hide')
         btn.innerText = 'Hide Password';
-        btn.style.backgroundColor = '#dadada';
-        btn.style.color = '#000';
     }
     else {
         input.type = 'password';
-        btn.style.backgroundColor = '#007bff';
-        btn.style.color = '#fff';
+        btn.classList.remove('hide');
         btn.innerText = 'Show Password';
     }
 })
